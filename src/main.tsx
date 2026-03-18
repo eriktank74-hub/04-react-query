5e0a6738ef3016f5f36925886dd742a60ec5283f
Erik <erik@Eriks-MacBook-Pro.local>
2026-03-18 15:47:28 +0200

    homework

--- src/main.tsx
+++ src/main.tsx
@@ -6,28 +6,9 @@ import App from './components/App/App';
 import {
   QueryClient,
   QueryClientProvider,
-  // useQuery,
 } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
-
-// function Example() {
-//   
-
-//   if (isPending) return 'Loading...'
-
-//   if (error) return 'An error has occurred: ' + error.message
-
-//   return (
-//     <div>
-//       <h1>{data.name}</h1>
-//       <p>{data.description}</p>
-//       <strong>👀 {data.subscribers_count}</strong>{' '}
-//       <strong>✨ {data.stargazers_count}</strong>{' '}
-//       <strong>🍴 {data.forks_count}</strong>
-//     </div>
-//   )
-// }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
